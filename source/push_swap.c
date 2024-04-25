3f77e44be33e9af1ac939c55fea6bb5fc1622836
VICTORxEVO <yassirsbai04@gmail.com>
2024-04-25 20:52:10 +0100

    Alpha V1.99

--- source/push_swap.c
+++ source/push_swap.c
@@ -1,5 +1,22 @@
 #include "../includes/push_swap.h"
 
+static void    print_stack(t_stack *head, char flag)
+{
+    if (flag == 'a')
+        printf("satck A\n+++++++++++++++++++++\n");
+    else
+        printf("satck B\n+++++++++++++++++++++\n");
+    t_stack *tmp = head;
+    int i = 0;
+    while(tmp)
+    {
+        printf("i -->> %d\tnumber -->> %d\n", i++, tmp->num);
+        printf("---------------------\n");
+        tmp = tmp->next;
+    }
+    printf("\n+++++++++++++END+++++++++++++\n");
+}
+
 int main (int ac, char *av[])
 {
     t_stack *a;
@@ -12,31 +29,15 @@ int main (int ac, char *av[])
     // if(!check_if_sorted(head[0]))
     //     main_sort(head);
     printf("is sorted -->>%d and max value is %d\n", check_if_sorted(head[0]), give_max(head, 'a'));
-    t_stack *tmp = head[0];
-    int i = 0;
-    while(tmp)
-    {
-        printf("%d   ", tmp->num);
-        printf("index is %d\n", i++);
-        printf("------------------\n");
-        tmp = tmp->next;
-    }
-    printf("\nAFTER PUSH\n");
+    printf("------\nAFTER PUSH\n--------\n");
+    push_to('b', head);
+    push_to('b', head);
     push_to('b', head);
     push_to('b', head);
     swap('a', head);
+    swap('b', head);
+    print_stack(head[0], 'a');
+    print_stack(head[1], 'b');
     // swap('b', head);
-    t_stack *tmp1 = head[0];
-    int i1 = 0;
-    while(tmp1)
-    {
-        printf("%d   ", tmp1->num);
-        printf("index is %d\n", i1++);
-        printf("------------------\n");
-        tmp1 = tmp1->next;
-    }
-    printf("\n\n");
-    printf("stack b have -->> %d", head[1]->num);
-
     clear_stack(head, 'S');
 }
