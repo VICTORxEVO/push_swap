e4e11b7fcf768641f5170a88479361f61f4326e4
VICTORxEVO <yassirsbai04@gmail.com>
2024-04-28 10:53:23 +0100

    Alpha V2.5

--- source/algo+insructions_func/sort_infinite.c
+++ source/algo+insructions_func/sort_infinite.c
@@ -1,6 +1,12 @@
 #include "../../includes/push_swap.h"
 
-static void    set_targets(t_stack **head)
+void    confirme_target(t_stack **to_set, t_stack **doubtful_target, int *nearest_number)
+{
+        (*to_set)->target = *doubtful_target;
+        *nearest_number = (*doubtful_target)->num;
+}
+
+static void    set_a_targets(t_stack **head)
 {
     t_stack *to_set;
     t_stack *doubtful_target;
@@ -11,26 +17,24 @@ static void    set_targets(t_stack **head)
     nearest_bellow = INT_MIN;
     while(to_set)
     {
+        to_set->target = NULL;
         while(doubtful_target)
-        {  
+        {
             if (doubtful_target->num > nearest_bellow && doubtful_target->num < to_set->num)
-            {
-                to_set->target = doubtful_target;
-                nearest_bellow = doubtful_target->num;
-                printf("inside loop and target->num is %d", to_set->target->num);
-            }
+                confirme_target(&to_set, &doubtful_target, &nearest_bellow);
             doubtful_target = doubtful_target->next;
         }
+        if (!to_set->target)
+            to_set->target = give_max('b', head);
         to_set = to_set->next;
+        doubtful_target = head[1];
+        nearest_bellow = INT_MIN;
     }
 }
-
-
-
 
 void    sort_infinite(t_stack **head, unsigned int size)
 {
     push_to('b', head);
     push_to('b', head);
-    set_targets(head);
+    set_a_targets(head);
 }
